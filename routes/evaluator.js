8c4e3c3ec7669edd804425a60005008a7b15c153
Chrisnetto <cscale@hijoresources.com>
2022-01-26 14:33:28 +0800

    added edit in evaluator

--- routes/evaluator.js
+++ routes/evaluator.js
@@ -1,32 +1,32 @@
-
 var express = require("express");
-var router  = express.Router();
-var Form = require("../models/form")
+var router = express.Router();
+var Form = require("../models/form");
 
+router.get("/evaluator", async function (req, res) {
+  var forms = await Form.find({});
+  console.log(forms);
+  res.json(forms);
+});
 
-router.get("/evaluator", async function (req, res){
-	var forms = await Form.find({})
-	console.log(forms)
-	res.json(forms)
-})
+router.post("/evaluator", async function (req, res) {
+  var forms = req.body;
 
-router.post("/evaluator", async function (req, res){
-	var forms = req.body
-	
-	for (var i = 0; i < forms.length; i++){
-		 var sendForm = new Form({form:forms[i]})
-         await sendForm.save()
-	}
-	res.json("online")
-})
+  for (var i = 0; i < forms.length; i++) {
+    var sendForm = new Form({ form: forms[i] });
+    await sendForm.save();
+  }
+  res.json("online");
+});
 
-router.get("/evaluator/:id", async function (req, res){
-	
-	var form = await Form.findById(req.params.id)
-	res.json(form)
-})
+router.get("/evaluator/:id", async function (req, res) {
+  var form = await Form.findById(req.params.id);
+  res.json(form);
+});
 
-
+router.post("/evaluator/edit/:formId", async function (req, res) {
+  await Form.findByIdAndUpdate(req.params.formId, { form: req.body });
+  res.json("Form updated");
+});
 
 // exports
 module.exports = router;
